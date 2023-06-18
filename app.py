14b81d7f812ee6b5ea5e4f5eef3ac2ed0b2b6f6c
Luis <luiselias0247@gmail.com>
2023-06-17 21:31:21 -0500

    logs

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask, request, Response
 import ocrmypdf
 import os
 import requests
+import logging
 
 app = Flask(__name__)
 
 # create a counter to avoid overwriting files
 file_count = 0
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# create a logger
+logger = logging.getLogger(__name__)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -34,6 +41,7 @@ def upload_file():
     # run OCR on the file
     try:
         ocrmypdf.ocr(input_path, output_path, deskew=True)
+        logger.info(f'OCR complete for file {file_count}')
     except Exception as e:
         return f'An error occurred during OCR: {str(e)}', 500
 
@@ -43,14 +51,17 @@ def upload_file():
         url = 'https://7c83le1kbc.execute-api.us-east-1.amazonaws.com/upload'
         
         response = requests.post(url,data=data)
+        logger.info(f'File {file_count} uploaded to S3')
         if response.status_code != 200:
             return f'An error occurred uploading the file: {response}', 500
 
     # delete the original file
     os.remove(input_path)
+    logger.info(f'File input {file_count} deleted from local storage')
 
     # delete the output file
     os.remove(output_path)
+    logger.info(f'File output {file_count} deleted from local storage')
 
     # return the response
     respond = Response(response.text, status=200, mimetype='application/json')
